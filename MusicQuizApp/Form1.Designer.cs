838dd71318704bc35a59452eaf25ea7cd40f689a
mfarhan08a <farhanputra.farhan8@gmail.com>
2022-12-07 17:17:53 +0700

    rename form1 to music quiz app

--- MusicQuizApp/Form1.Designer.cs
+++ MusicQuizApp/Form1.Designer.cs
@@ -725,7 +725,7 @@
             this.Controls.Add(this.pnl_loading);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Music Quiz App";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.pnl_welcome.ResumeLayout(false);
